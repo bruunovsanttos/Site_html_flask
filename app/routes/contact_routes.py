46d9359d887b6ed23f51ae03923cbe0674fdb94b
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-04-22 22:56:53 -0300

    adição de codigos de rotas e ajustes nas nomenclaturas dos codigos de contatc no html para funcionamento correto

--- app/routes/contact_routes.py
+++ app/routes/contact_routes.py
@@ -1,1 +1,33 @@
-#rota de contato para ler a api
+from flask import Blueprint, request
+from app.database.db import db
+from app.models.contato import Contact
+
+contato_bp = Blueprint("contato", __name__)
+
+
+@contato_bp.route("/contato", methods=["POST"])
+def receber_contato():
+
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    email = request.form.get("email")
+    mensagem = request.form.get("mensagem")
+
+
+    if not nome or not telefone or not email or not mensagem:
+        return "Preencha todos os campos obrigatórios!", 400
+
+
+    novo_contato = Contact(
+        nome=nome,
+        telefone=telefone,
+        email=email,
+        mensagem=mensagem
+    )
+
+
+    db.session.add(novo_contato)
+    db.session.commit()
+
+
+    return "Mensagem enviada com sucesso!"
