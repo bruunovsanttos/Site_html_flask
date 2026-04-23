5451c48f6bf8f11dee6c98a51227614ed5aff3d0
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-04-23 13:58:43 -0300

    adição de rotas de index e project para litura correta do projeto

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
 
+@main_bp.route("/")
+def home():
+    return render_template("index.html")
 
+@main_bp.route("/projetos")
+def projetos():
+    return render_template("project.html")
 @main_bp.route("/")
 def home():
     return render_template("contact.html")
